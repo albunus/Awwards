1b29339ddf8c765d169c62348da6704894b372e3
albunus <albunus.nyalita@student.moringaschool.com>
2021-12-12 10:45:50 +0300

    added project class

--- award/models.py
+++ award/models.py
@@ -37,4 +37,47 @@ class Profile(models.Model):
     def search_profile(cls, name):
         return cls.objects.filter(user__username__icontains=name).all()
     
+class Project(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=250)
+    description = models.TextField()
+    image = CloudinaryField("image")
+    url = models.URLField(blank=True)
+    location = models.CharField(max_length=100, default="Machakos")
+    date = models.DateTimeField(auto_now_add=True, null=True)
+
+    @classmethod
+    def search_by_title(cls, search_term):
+        projects = cls.objects.filter(title__icontains=search_term)
+        return projects
+
+    @classmethod
+    def get_project_by_id(cls, id):
+        project = cls.objects.get(id=id)
+        return project
+
+    @classmethod
+    def get_all_projects(cls):
+        projects = cls.objects.all()
+        return projects
+
+    @classmethod
+    def get_all_projects_by_user(cls, user):
+        projects = cls.objects.filter(user=user)
+        return projects
+
+    # update project
+    def update_project(self, **kwargs):
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+        self.save()
+
+    def save_project(self):
+        self.save()
+
+    def delete_project(self):
+        self.delete()
+
+    def __str__(self):
+        return self.title
 
